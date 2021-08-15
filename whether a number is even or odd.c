2fff6b79df80b65db1de08022a857966cbb43a23
samra-chaudhary <85254565+samra-chaudhary@users.noreply.github.com>
2021-08-15 15:27:10 +0530

    Add files via upload

---
+++ whether a number is even or odd.c
@@ -0,0 +1,12 @@
+//samra chaudhary
+#include <stdio.h>
+void main()
+{
+    int number;
+    printf("enter the number");
+    scanf("%d",&number);
+    if((number % 2)==0) //conditional operator is used
+    printf(" the number is even");
+    else
+    printf("the number is odd");
+}
