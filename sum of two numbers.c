ef0a903e693d90943f8b91f7be8b0ebffbbf4307
samra-chaudhary <85254565+samra-chaudhary@users.noreply.github.com>
2021-08-15 14:01:02 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,15 @@
+  // name: samra chaudhary
+  #include <stdio.h>
+int main()
+{
+  int x, y, z;
+
+  printf("Enter two numbers to add\n");
+  scanf("%d%d", &x, &y); //taking user input
+
+  z = x + y;
+
+  printf("Sum of the numbers = %d\n", z);
+
+  return 0;
+}
