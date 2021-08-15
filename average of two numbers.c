35574168e7589a06e9bb03d3c6c27121039b67d3
samra-chaudhary <85254565+samra-chaudhary@users.noreply.github.com>
2021-08-15 17:16:39 +0530

    Add files via upload

---
+++ average of two numbers.c
@@ -0,0 +1,13 @@
+//samra chaudhary
+#include<stdio.h>
+void main()
+{int a,b,c;
+ printf("enter the value of a :");
+ scanf("%d",&a);//taking input from the  user 
+ printf("enter the value of b :");
+ scanf("%d",&b);
+ c=(a+b)/2;
+ printf("the answer is %d",c);
+ 
+
+}
