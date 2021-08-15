d4d6db9567eff213acb16acc952f312dc123f6b9
samra-chaudhary <85254565+samra-chaudhary@users.noreply.github.com>
2021-08-15 15:05:00 +0530

    Add files via upload

---
+++ accept two integers, whether they are equal or not.c
@@ -0,0 +1,16 @@
+//samra chaudhary
+#include<stdio.h>
+void main()
+{
+    int m,n;
+    printf("enter the value of m");
+    scanf("%d", &m);
+    printf("enter the value of n");
+    scanf("%d",&n);
+    if(m==n)//conditional operator
+    printf("the numbers are equal");
+    else
+    printf("the numbers are not equal");
+
+    
+}
