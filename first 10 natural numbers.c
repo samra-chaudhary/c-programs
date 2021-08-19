ae77c6aec2152a0e1b27dbb2840b1fb9da7b8305
samra-chaudhary <85254565+samra-chaudhary@users.noreply.github.com>
2021-08-19 16:37:39 +0530

    Add files via upload

---
+++ first 10 natural numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int i;
+    
+    i=1;
+    while(i<=10)//condition
+    {
+        printf("\n%d",i);
+    i=i+1;
+    }
+    
+
+    return 0;
+}
---
+++ sum of first 10 natural numbers.c
@@ -0,0 +1,23 @@
+//samra chaudhary
+#include <stdio.h>
+
+int main()
+
+{
+    int i,sum;
+    
+    i=1;//initialisation
+    sum=0;
+    
+    while(i<=10)//condition
+    { sum=sum+i;
+    i=i+1;
+    }
+    
+        printf("\n sum = %d",sum);
+    
+
+    
+
+    return 0;
+}
